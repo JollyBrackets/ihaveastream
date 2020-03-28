4a3801343c6092e97b5693a6efdec2fec89e27ac
Nicolas Zahnd <nicolas.z.zahnd@ch.pwc.com>
2020-03-28 16:17:02 +0100

    Add profile

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="black" dark>
       <v-layout row align-center>
-        <v-flex xs3 pl-4>
+        <v-flex xs3 pl-4 @click="$router.push('/')">
           <v-img contain :height="40" :width="40" :src="require('@/assets/logo.svg')" />
         </v-flex>
         <v-flex xs6>
@@ -18,8 +18,12 @@
           </v-text-field>
         </v-flex>
         <v-flex xs3 text-right pr-4>
-          <v-btn text class="text-none red">
+          <v-btn text class="text-none red" @click="$router.push('create-stream')">
             I HAVE A STREAM
+          </v-btn>
+          
+          <v-btn fab small class="ml-3" @click="$router.push('profile')">
+            <v-icon>mdi-account-circle-outline</v-icon>
           </v-btn>
         </v-flex>
       </v-layout>
---
+++ src/views/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+ <v-container>
+    <v-layout row v-if="user">
+        <v-flex xs4 class="pa-2">
+            <v-avatar size="200">
+                <v-img src="https://source.unsplash.com/200x200/?portrait,woman">
+                    <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                        <v-progress-circular indeterminate color="red lighten-3" />
+                        </v-row>
+                    </template>
+                </v-img>
+            </v-avatar>
+            <p class="title">{{ user.name.first }} - {{ user.name.last }}</p>
+        </v-flex>
+        <v-flex xs4 class="pa-2">
+            <pre>
+                {{ user }}
+            </pre>
+        </v-flex>
+    </v-layout>
+
+    <v-row class="fill-height ma-0" align="center" justify="center" v-else>
+        <v-progress-circular indeterminate color="red lighten-3" />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Profile',
+  data: () => ({
+      user: null
+  }),
+  async created () {
+    const response = await axios.get('https://randomuser.me/api/')
+    this.user = response.data.results[0]
+    console.log('user', this.user)
+  }
+}
+</script>
