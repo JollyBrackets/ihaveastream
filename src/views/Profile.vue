f644e054b4de508df4742401aa815acc81c4069f
Nora Hjördis Moser <mosern@tcd.ie>
2020-03-28 19:06:16 +0100

    attempt at a table

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -14,27 +14,56 @@
             <p class="title">{{ user.name.first }} {{ user.name.last }}</p>
             <p class=".body-1">{{ user.name.title }} {{ user.gender }}</p>
         </v-flex>
+
+        <div id="vue-root">
+            <datatable :columns="columns" :data="rows"></datatable>
+        </div>
+
         <v-flex xs4 class="pa-2">
             <pre>
                 <p class="title">Subscription details</p>
-                <b-card title="Dancing">
-                    <b-card-text>
-                    <b>Nr. 1 Dance school</b>
-                    </b-card-text>
-                    <b-card-text>
-                    <b>Hip-Hop</b>
-                    </b-card-text>
-                    <a href="#" class="card-link">Card link</a>
-                    <br>
-                    <b-card-text>
-                    <b>Fitness all day</b>
-                    </b-card-text>
-                    <b-card-text>
-                    <b>Zumba</b>
-                    </b-card-text>
-                    <a href="#" class="card-link">Card link</a>
-                    <br>
-                </b-card>
+                <v-card
+                max-width="500"
+                class="mx-auto"
+                outlined
+                >
+                <v-list-item>
+                    <v-list-item-avatar color="grey"></v-list-item-avatar>
+                    <v-list-item-content>
+                    <v-list-item-title class="headline">Hip-Hop 2</v-list-item-title>
+                    <v-list-item-subtitle>by Best Dance School Zürich</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            
+                <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+                    height="194"
+                    width="500"
+                ></v-img>
+            
+                <v-card-actions>
+                    <v-btn
+                    text
+                    color="deep-purple accent-3"
+                    >
+                    Read
+                    </v-btn>
+                    <v-btn
+                    text
+                    color="deep-purple accent-4"
+                    >
+                    Bookmark
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                    </v-btn>
+                </v-card-actions>
+                </v-card>
+                
                 
                 <p class=".subtitle-1">Drinks and chats</p>
                 <p class=".body-1">{Streamer Name 1}<br>{Course Name 1}</p>
@@ -62,4 +91,36 @@ export default {
     this.user = response.data.results[0]
   }
 }
+
+import Vue from 'vue';
+
+new Vue({
+    el: '#vue-root',
+    data: {
+        columns: [
+            {label: 'id', field: 'id'},
+            {label: 'Username', field: 'user.username', headerClass: 'class-in-header second-class'},
+            {label: 'First Name', field: 'user.firstName'},
+            {label: 'Last Name', field: 'user.lastName'},
+            {label: 'Email', field: 'user.email'},
+            {label: 'Address', representedAs: ({address, city, state}) => `${address}<br />${city}, ${state}`, interpolate: true}
+        ],
+        rows: [
+
+            {
+                id: 1,
+                user: {
+                    username: "dprice0",
+                    firstName: "Daniel",
+                    lastName: "Price",
+                    email: "dprice0@blogs.com"
+                },
+                address: "3 Toban Park",
+                city: "Pocatello",
+                state: "Idaho"
+            }
+        ]
+    }
+});
+
 </script>
