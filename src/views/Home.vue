a8bcbbc193eeeb9f4ebe4c3857127535dd409ee6
Nicolas Zahnd <nicolas.z.zahnd@ch.pwc.com>
2020-03-29 15:27:11 +0200

    Random home messages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,8 @@
     <v-container>
       <v-row class="text-center white--text">
         <v-col>
-          <h1 class="my-4">Stay connected despite Corona Kack</h1>
-          <p class="font-weight-bold">Bla bla Bla bla Bla bla Bla blaBla blaBla bla</p>
+          <h1 class="my-4">{{ heroTitle[Math.floor(Math.random() * heroTitle.length)] }}</h1>
+          <p class="font-weight-bold">{{ heroMessage[Math.floor(Math.random() * heroMessage.length)] }}</p>
           <v-btn text class="text-none primary" @click="$router.push('create-stream')">
             I HAVE A STREAM
           </v-btn>
@@ -96,6 +96,28 @@ export default {
   components: { ExploreCard, FilterBar },
   name: 'Home',
   data: () => ({
+    heroTitle: [
+      'Connected despite Corona',
+      'Time to connect',
+      'Connecting Streamers and Streamees',
+      'Live streams around you'
+    ],
+    heroMessage: [
+      "Transition your business into streaming",
+      "Help your business with a stream",
+      "Discover your streamed self",
+      "De-quarantine your business through streaming",
+      "Reach your students via stream",
+      "Stay connected to your customers",
+      "Don’t quarantine your passions",
+      "Don’t quarantine your curiosity",
+      "Connect digitally with everyone around you",
+      "Don’t lose your routine",
+      "Your new spanish teacher is waiting for you",
+      "Have you ever dreamed of dancing in your living room?",
+      "Bringing the dance floor to your living room!",
+      "I know you want to improve your drawing skills"
+    ]
   })
 }
 </script>
