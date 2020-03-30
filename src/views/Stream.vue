2dcced88849760838d39b7c9ca7c119e7353264b
Martin Eigenmann <github@eigenmannmartin.ch>
2020-03-30 16:54:55 +0200

    uses iframe for videos

--- src/views/Stream.vue
+++ src/views/Stream.vue
@@ -2,9 +2,13 @@
   <v-container>
     <v-row v-if="stream">
       <v-col cols="9">
-        <video v-if="liveSession" controls style="max-width: 100%">
-          <source :src="liveSession.url" />
-        </video>
+        <template v-if="liveSession">
+          <iframe 
+            style="border: none; width: 100%; min-height: 600px;" :src="liveSession.url"
+            frameborder="0"
+          >
+          </iframe>
+        </template>
 
         <v-img
           v-else
@@ -158,6 +162,11 @@ export default {
 
         return moment(session.start).isBefore(moment().subtract(duration, 'minutes'))
       })
+    }
+  },
+  methods: {
+    resizeIframe(obj) {
+      console.log(obj.contentWindow.document.documentElement.scrollHeight)
     }
   },
   async created() {
